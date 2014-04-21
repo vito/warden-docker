d2e94eef3aa94c95c596399f55c7510d3cff32b9
Alex Suraci <suraci.alex@gmail.com>
2014-04-20 18:35:18 -0700

    re-add skeleton with rootfs_raw support

---
+++ Makefile
@@ -0,0 +1,8 @@
+all:
+	cd src && make clean all
+	cp src/wsh/wshd skeleton/bin
+	cp src/wsh/wsh skeleton/bin
+	cp src/oom/oom skeleton/bin
+	cp src/iomux/iomux-spawn skeleton/bin
+	cp src/iomux/iomux-link skeleton/bin
+	cp src/repquota/repquota bin
---
+++ skeleton/lib/hook-child-after-pivot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+[ -n "$DEBUG" ] && set -o xtrace
+set -o nounset
+set -o errexit
+shopt -s nullglob
+
+cd $(dirname $0)/../
+
+source ./lib/common.sh
+
+mkdir -p /dev/pts
+mount -t devpts -o newinstance,ptmxmode=0666 devpts /dev/pts
+ln -sf pts/ptmx /dev/ptmx
+
+mkdir -p /proc
+mount -t proc none /proc
+
+hostname $id
+
+ip address add 127.0.0.1/8 dev lo
+ip link set lo up
+
+ip address add $network_container_ip/30 dev $network_container_iface
+ip link set $network_container_iface mtu $container_iface_mtu up
+
+ip route add default via $network_host_ip dev $network_container_iface
+
+if [ -e /etc/seed ]; then
+  . /etc/seed
+fi
